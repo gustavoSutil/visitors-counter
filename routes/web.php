8222b0dee5593650a6a0a3b2e369f0f29d8d1ac7
Gustavo Guilherme Sutil Alberton <gustavo.alberton@expressosaomiguel.com.br>
2024-02-07 17:37:45 -0300

    feat: add first sctructure

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/{username}', [App\Http\Controllers\ProfileCountController::class, 'index'])->name('countprofile');
